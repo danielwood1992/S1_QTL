c302f3a5d7e3d6557ad61c49431c4cf0b90b04fc
danielwood1992 <bssc1d@bangor.ac.uk>
2022-03-15 08:47:27 +0000

    updated to work with all crosses

--- LM_4.7_mergeF2sParents.sh
+++ LM_4.7_mergeF2sParents.sh
@@ -1,35 +1,33 @@
 #!/bin/bash --login
-#SBATCH -o /scratch/b.bssc1d/Linkage_Mapping/logs/batch_LM_4.5B_%A.out.txt
-#SBATCH -e /scratch/b.bssc1d/Linkage_Mapping/logs/batch_LM_4.5B_%A.err.txt
+#SBATCH -o /scratch/b.bssc1d/Linkage_Mapping/logs/batch_LM_4.7_%A.out.txt
+#SBATCH -e /scratch/b.bssc1d/Linkage_Mapping/logs/batch_LM_4.7_%A.err.txt
 #SBATCH --ntasks=1
 #SBATCH --time=12:00:00
 #SBATCH --partition=htc
 
-#Just sh it, won't take very long. Nice.
+#Run on the login node, only takes a second.
+#NOTE: Check the $outdir/${cross}_ParentsPlusF2s_0.05/README.txt: make sure the megred files are what you want.
+
+#Inputs: filtered combined parent vcf (from LM_4.5A)
+#        filtered combined F2 vcf (from LM_4.6)
+#Function: combines these files, taking only files with values in both
+#parents and the F2s...
+#Outputs : $outdir/${cross}_Parents_F2s.vcf
+#        e.g. LM1_QA/QA_Parents_F2s.vcf
 
 dir1="/scratch/b.bssc1d/Linkage_Mapping/LM_4.2_ParentCalls";
 dir2="/scratch/b.bssc1d/Linkage_Mapping/QB_Raw";
 progdir="/scratch/b.bssc1d/Linkage_Mapping";
 
+cross="QA";
+#cross="QB";
+#cross="QCE";
+
 #Output of LM_4.5A
-parent_file="/home/b.bssc1d/scripts/Linkage_Mapping/LM1_${cross}/${cross}.Parents.merged.filt1.bcf.gz";
+parent_file="/scratch/b.bssc1d/Linkage_Mapping/LM1_${cross}/${cross}.Parents.merged.filt1.bcf.gz";
 #Output of LM_4.6
-F2_file="/home/b.bssc1d/scripts/Linkage_Mapping/LM1_${cross}/${cross}.merged_snps_lowmem.filter2.vcf.0.05";
-
-#QA
-#parents="/home/b.bssc1d/scripts/Linkage_Mapping/QA_Parents.txt";
-#name="QA_Parents"
-#dir1="/scratch/b.bssc1d/6Pop_Resequencing";
-#dir2="/scratch/b.bssc1d/Linkage_Mapping";
-#QA_file="QA_Parents.merged.filtered3.bcf.gz";
-#Parent_File="merged_snps_lowmem.filter2.bcf.gz";
-
-#QB
-#parents="/home/b.bssc1d/scripts/Linkage_Mapping/QA_Parents.txt";
-#name="QB_Parents"
-#parent_file="/scratch/b.bssc1d/Linkage_Mapping/LM_4.2_ParentCalls/QB_Parents.merged.filt1.bcf.gz";
-#F2_file="/scratch/b.bssc1d/Linkage_Mapping/QB_Raw/QB.merged_snps_lowmem.filter2.bcf.gz";
-F2_file="/scratch/b.bssc1d/Linkage_Mapping/QB_Raw/QB.merged_snps_lowmem.filter2.vcf.0.05.bcf.gz";
+F2_file="/scratch/b.bssc1d/Linkage_Mapping/LM1_${cross}/${cross}.merged_snps_lowmem.filter2.vcf.0.05.bcf.gz";
+outdir="/scratch/b.bssc1d/Linkage_Mapping/LM1_${cross}";
 
 module load bcftools
 module load parallel
@@ -38,10 +36,13 @@ bcftools index -f $parent_file;
 bcftools index -f $F2_file;
 
 #bcftools index -f $dir2/$Parent_File;
-bcftools isec -p $dir1/QBplusF2s_0.05 $F2_file $parent_file;
-bcftools view $dir1/QBplusF2s_0.05/0002.vcf -Ob -o $dir1/QBplusF2s_0.05/0002.bcf.gz;
-bcftools view $dir1/QBplusF2s_0.05/0003.vcf -Ob -o $dir1/QBplusF2s_0.05/0003.bcf.gz;
-bcftools index $dir1/QBplusF2s_0.05/0002.bcf.gz;
-bcftools index $dir1/QBplusF2s_0.05/0003.bcf.gz;
+bcftools isec -p $outdir/${cross}_ParentsPlusF2s_0.05 $F2_file $parent_file;
 
-bcftools merge $dir1/QBplusF2s_0.05/0002.bcf.gz $dir1/QBplusF2s_0.05/0003.bcf.gz > $dir1/QB_Parents_F2s.vcf && bcftools stats -s - $dir1/QB_Parents_F2s.vcf > $dir1/QB_Parents_F2s.vcf.stats;
+bcftools view $outdir/${cross}_ParentsPlusF2s_0.05/0002.vcf -Ob -o $outdir/${cross}_ParentsPlusF2s_0.05/0002.bcf.gz;
+
+bcftools view $outdir/${cross}_ParentsPlusF2s_0.05/0003.vcf -Ob -o $outdir/${cross}_ParentsPlusF2s_0.05/0003.bcf.gz;
+
+bcftools index $outdir/${cross}_ParentsPlusF2s_0.05/0002.bcf.gz;
+bcftools index $outdir/${cross}_ParentsPlusF2s_0.05/0003.bcf.gz;
+
+bcftools merge $outdir/${cross}_ParentsPlusF2s_0.05/0002.bcf.gz $outdir/${cross}_ParentsPlusF2s_0.05/0003.bcf.gz > $outdir/${cross}_Parents_F2s.vcf && bcftools stats -s - $outdir/${cross}_Parents_F2s.vcf > $outdir/${cross}_Parents_F2s.vcf.stats;
